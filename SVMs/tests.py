e457c19a525982e15799cac39875eb1a2ffa4ad5
Álvaro Barbero Jiménez <albarjip@gmail.com>
2018-06-09 15:53:36 +0200

    Tkinter test for SVM environment

--- SVMs/tests.py
+++ SVMs/tests.py
@@ -7,8 +7,19 @@ import scipy
 import numpy
 
 from scipy.misc import imread
+from sklearn.externals.six.moves import xrange
 
 from sklearn import svm
+
+matplotlib.use('TkAgg')
+import tkinter as Tk
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+from matplotlib.contour import ContourSet
+from matplotlib.lines import Line2D
+
 
 def test_svm():
     """Trains a SVM"""
@@ -17,4 +28,9 @@ def test_svm():
     clf = svm.SVC()
     clf.fit(X, y)  
     clf.predict([[2., 2.]])
+
+
+def test_tkinter():
+    """Tests that a Tk inter object can be created"""
+    root = Tk.Tk()
 

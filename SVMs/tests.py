ea9b25052a1c1db8c2cfb3079e9e0c792186e6ed
Álvaro Barbero Jiménez <albarjip@gmail.com>
2018-06-15 19:37:37 +0200

    Added proxTV package

--- SVMs/tests.py
+++ SVMs/tests.py
@@ -5,7 +5,7 @@ matplotlib.use('TkAgg')
 import sklearn
 import pandas
 import scipy
-import numpy
+import numpy as np
 
 from scipy.misc import imread
 from sklearn.externals.six.moves import xrange
@@ -20,6 +20,8 @@ from matplotlib.figure import Figure
 from matplotlib.contour import ContourSet
 from matplotlib.lines import Line2D
 
+import prox_tv as ptv
+
 
 def test_svm():
     """Trains a SVM"""
@@ -28,4 +30,18 @@ def test_svm():
     clf = svm.SVC()
     clf.fit(X, y)  
     clf.predict([[2., 2.]])
+
+
+def test_proxtv():
+    """Tests proxTV"""
+    N = 1000
+    s = np.zeros((N,1))
+    s[int(N/4):int(N/2)] = 1
+    s[int(N/2):int(3*N/4)] = -1
+    s[int(3*N/4):int(-N/8)] = 2
+
+    n = s + 0.5*np.random.rand(*np.shape(s))
+
+    lam=20
+    f = ptv.tv1_1d(n,lam)
 
